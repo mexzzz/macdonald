2a80057b3897c2394feae55b5a1992e4fad2ca5a
Aryz0n <aryz0n@outlook.com>
2023-03-30 14:31:04 +0200

    I want to die

---
+++ php/index_view2.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+                content="width=device-width, user-scalable=no, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Eindopdracht</title>
+        <link href="eind.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+        <h1>Voer hier uw gegevens in:</h1>
+        <form name="InschrijfForm" method="post" action="index2_verwerk.php">
+        <p>Naam: <input type="text" name="naam" id="naam"required/></p>
+        <p>Studentnummer: <input type="text" name="studentnummer" id="studentnummer"required/></p>
+        <p>Klas: <input type="text" name="klas" id="klas"required/></p>
+        <p>Leerlijn 1: <input type="text" name="ll1" id="ll1" required/></p>
+        <p>Cijfer Leerlijn 1: <input type="text" name="cll1" id="cll1"required/></p>
+        <p>Leerlijn 2: <input type="text" name="ll2" id="ll2" required/></p>
+        <p>Cijfer Leerlijn 2: <input type="text" name="cll2" id="cll2"required/></p>
+        <p>Leerlijn 3: <input type="text" name="ll3" id="ll3" required/></p>
+        <p>Cijfer Leerlijn 3: <input type="text" name="cll3" id="cll3"required/></p>
+        <p>Leerlijn 4: <input type="text" name="ll4" id="ll4" required/></p>
+        <p>Cijfer Leerlijn 4: <input type="text" name="cll4" id="cll4"required/></p>
+
+        <input type="submit" name="submit" id="submit" value="Verzenden"/>
+        </form>
+    </body>
+    </html>
+
+    <!-- De HTML pagina. Hier kom je als eerst en vul je alles in. -->
